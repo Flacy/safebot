ceb24b8c75a0d8972ed5f3c3a04b170f8a830a7c
lunte <any@lunte.dev>
2024-03-12 06:44:42 +0300

    fix: make chat t_id unique

--- safebot/database/models.py
+++ safebot/database/models.py
@@ -10,7 +10,7 @@ class BaseModel(peewee.Model):
 
 class Chat(BaseModel):
     # Telegram chat ID
-    t_id = peewee.BigIntegerField()
+    t_id = peewee.BigIntegerField(unique=True)
     # This mode means that every action or error will not be displayed in the chat
     silent_mode = peewee.BooleanField(default=False)
     # This mode literally repeats the bot message, but without any advertisements
