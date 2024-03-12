c11ff6ec3e78e0476c30be822ae194447327a913
lunte <any@lunte.dev>
2024-03-12 06:44:20 +0300

    feat: add private message handler

--- safebot/handlers/chat/private.py
+++ safebot/handlers/chat/private.py
@@ -1,6 +1,84 @@
-from handlers import MessageProtocol
+from math import ceil
+from typing import Dict, Any
+
+from pyrogram.errors import (
+    FloodWait,
+    InviteHashExpired,
+    UserAlreadyParticipant,
+)
+from pyrogram.types import Chat
+
+from client import client
+from detect.link import Link
+from handlers import MessageProtocol, database
+from handlers.emitter import Emitter
+from helpers import retrieve_urls
+from logger import logger
 
 
 class PrivateMessage(MessageProtocol):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self._emit: Emitter = Emitter(self.message)
+
+    def _retrieve_invite_link(self) -> str | None:
+        """
+        Retrieves the first link from the message entities.
+
+        :return: Text link, if found
+        """
+        for url in retrieve_urls(self.message.text, self.message.entities):
+            return url
+
+    async def _join_chat(self, url: str) -> Chat | None:
+        """
+        Enters the chat through the provided link.
+        If unsuccessful, a response message will be sent with a description
+        of the error.
+
+        :param url: Text link.
+        :return: On success a ``Chat`` object.
+        """
+        fmt: Dict[str, Any] = {}
+
+        try:
+            return await client.join_chat(url)
+        except UserAlreadyParticipant:
+            locale_key = "already_in_chat"
+        except InviteHashExpired:
+            # Actually, this error occurs not only when the link is invalid.
+            # It also happens if the bot failed to enter the chat due to some problem.
+            # For example, if it's banned from the chat.
+            locale_key = "invite_link_expired"
+        except FloodWait as e:
+            # This happens when we entered or attempted to enter a chat too frequently.
+            # Normally, the cooldown lasts form 5 to 20 minutes,
+            # so we explicitly inform that everything is fine,
+            # just a temporary restriction is in place.
+            locale_key = "error_flood"
+            fmt["minutes"] = ceil(e.value / 60)
+
+        # Raise any other errors.
+        # It's not necessary for the user to know about an unknown error.
+        # But it will be very helpful for debugging.
+
+        await self._emit.send(locale_key, **fmt)
+
     async def process(self) -> None:
-        pass
+        """
+        Scan the message for a link (only the first found is taken),
+        and checks it for compliance with an invitation link.
+        If it's an invitation, attempt to enter the chat.
+        Otherwise, the message is skipped.
+
+        Private chat simultaneously serves as feedback, so we don't send any
+        unnecessary information.
+        """
+        if (
+            (url := self._retrieve_invite_link())
+            and Link(url).is_invite
+            and (chat := await self._join_chat(url))
+        ):
+            await database.create_or_skip(chat.id)
+            logger.info(f"Joined the chat: {chat.title} ({chat.id=})")
--- safebot/handlers/database.py
+++ safebot/handlers/database.py
@@ -1,5 +1,15 @@
+from peewee_async import IntegrityErrors  # noqa
+
 from database.client import manager
 from database.models import Chat
+
+
+async def create_or_skip(chat_id: int) -> None:
+    try:
+        await manager.create(Chat, t_id=chat_id)
+    except IntegrityErrors:
+        # Ignore errors if record already exists
+        pass
 
 
 async def is_silent_mode(chat_id: int) -> bool:
